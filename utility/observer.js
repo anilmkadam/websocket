2ab6ceb5b55ee15fc7b08b154ef5c951cfb00ae1
Anil Kadam <anilkadam210@gmail.com>
2021-12-11 11:40:11 +0530

    feat: Initial setup

---
+++ utility/observer.js
@@ -0,0 +1,41 @@
+const chokidar = require("chokidar");
+//const readLastLines = require("read-last-lines");
+const EventEmitter = require("events").EventEmitter;
+const getLastLine = require("./readListLines").getLastLine;
+
+class Observer extends EventEmitter {
+    constructor() {
+        super();
+    }
+
+    watchFile(fileName, maxReadLines = 1) {
+        return new Promise((resolve, reject) => {
+            try {
+                console.log(
+                    `[${new Date().toLocaleString()}] Watching for file changes on: ${fileName}`
+                  );
+
+                const watcher = chokidar.watch(fileName, { persistent: true });
+
+                watcher.on("all", async (event, filePath) => {
+                    console.log(
+                        `[${new Date().toLocaleString()}] ${filePath} has been updated.`
+                      );
+                    
+                    //const logContent = await readLastLines.read(filePath, maxReadLines);
+                    const logContent = await getLastLine(fileName, 10);
+                    this.emit("log-received", { 
+                        timestamp: new Date().toLocaleString(),
+                        metadata: logContent 
+                    });
+                });
+
+            } catch(error) {
+                console.log("Error occured => ", error.message);
+                reject(error);
+            }
+        });
+    }
+}
+
+module.exports = Observer;
